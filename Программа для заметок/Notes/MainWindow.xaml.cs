88922f3c9bfc2b447c87e331e74b1dc79be97645
KuzminOleg2pc2 <91515428+Ugwey2021@users.noreply.github.com>
2023-05-07 23:06:49 +0500

    Add files via upload

---
+++ Программа для заметок/Notes/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace Notes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateList();
+        }
+        private void UpdateList()
+        {
+            List<string> elements = new List<string>();
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            foreach (var s in files)
+            {
+                if (s.Contains(".txt")) elements.Add(Path.GetFileName(s));
+            }
+            lsView.ItemsSource = elements;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (cmb_box.SelectedValue != null)
+            {
+                if (cmb_box.SelectedValue.ToString().Contains("InkCanvas"))
+                {
+                    NoteWindow nw = new NoteWindow();
+                    nw.Show();
+                    this.Close();
+                }
+                else if (cmb_box.SelectedValue.ToString().Contains("Text"))
+                {
+                    NoteWindow nw = new NoteWindow();
+                    nw.Show();
+                    nw.txt.Visibility = Visibility.Visible;
+                    nw.inkCanvas.Visibility = Visibility.Collapsed;
+                    this.Close();
+                }
+            }
+        }
+
+        private void Select(object sender, SelectionChangedEventArgs e)
+        {
+            if (btn_delete.IsEnabled)
+            {
+                NoteWindow nw = new NoteWindow();
+                nw.Show();
+                FileStream fs = File.Open($"{lsView.SelectedItem.ToString()}", FileMode.Open);
+                StrokeCollection myStrk = new StrokeCollection(fs);
+                nw.inkCanvas.Strokes = myStrk;
+                fs.Close();
+                this.Close();
+            }
+            else
+            {
+                try {
+                    File.Delete($"{lsView.SelectedItem.ToString()}");
+                    
+                } catch { }
+                UpdateList();
+                btn_delete.IsEnabled = true;
+            } 
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            btn_delete.IsEnabled = false;
+        }
+
+        private void cmb_box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Программа для заметок/Notes/NoteWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Notes
+{
+    /// <summary>
+    /// Логика взаимодействия для NoteWindow.xaml
+    /// </summary>
+    public partial class NoteWindow : Window
+    {
+        public NoteWindow()
+        {
+            InitializeComponent();
+        }
+        CareTaker cr = new CareTaker();
+        int activeMemento = -1;
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = File.Open($"{NameTxt.Text}.txt", FileMode.OpenOrCreate);
+            inkCanvas.Strokes.Save(fs);
+            fs.Close();
+        }
+
+        private void SaveCare(object sender, RoutedEventArgs e)
+        {
+            cr.Memento.Add(new Memento(inkCanvas.EditingMode, new StrokeCollection(inkCanvas.Strokes)));
+            activeMemento = cr.Memento.Count()-1;
+        }
+
+        private void LoadCare(object sender, RoutedEventArgs e)
+        {
+            if (cr.Memento.Count != 0)
+            {
+                activeMemento--;
+                inkCanvas.Strokes = cr.Memento[activeMemento].strokes;  
+            }
+            else
+            {
+                MessageBox.Show("Снимков не существует!");
+            }
+            
+        }
+    }
+    class Memento
+    {
+        public InkCanvasEditingMode editMode { get; set; }
+        public StrokeCollection strokes { get; set; }
+        public Memento(InkCanvasEditingMode e, StrokeCollection s)
+        {
+            editMode = e;
+            strokes = s;
+        }
+    }
+    class CareTaker
+    {
+        public List<Memento> Memento { get; set; }
+        public CareTaker()
+        {
+            Memento = new List<Memento>();
+        }
+    }
+
+}
